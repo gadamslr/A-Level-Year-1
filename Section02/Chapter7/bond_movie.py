a06a1287f2be01f412ed93e2fb6dda7787ea10b0
gadamslr <gadams@longroad.ac.uk>
2016-11-08 22:05:20 +0000

    Create bond_movie.py

---
+++ Section02/Chapter7/bond_movie.py
@@ -0,0 +1,8 @@
+bondDict = {
+'2008': [	'Daniel Craig','Quantom of Solace',8.9,514.2 ],
+'2006':	[   'Daniel Craig',	'Casino Royale',3.4,581.5 ],
+'2002':	[   'Pierce Brosnan', 'Die Another Day',16.5,465.4 ] }
+
+for movie in bondDict:
+    print( bondDict[movie])
+
