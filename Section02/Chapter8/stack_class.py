525a7cd506d79e46ceb0dc9a264b8a7228ffa4f7
gadamslr <gadams@longroad.ac.uk>
2016-11-09 15:04:06 +0000

    Update stack_class.py

--- Section02/Chapter8/stack_class.py
+++ Section02/Chapter8/stack_class.py
@@ -20,5 +20,42 @@ class Stack:
 x = Stack()
 
 print(x.size())
-#Popping an empty stack.
-#print(x.pop())
+
+#Using these processes on an empty stack will cause errors
+"""#Popping an empty stack.
+print(x.pop())
+#Peeking at the top of a stack
+print(x.peek())
+"""
+print("x.push('Rod')| Push Rod onto the stack :")
+x.push("Rod")
+#####
+print("")
+#####
+print("x.push('Jane') | Push Jane onto the stack:")
+x.push("Jane")
+#####
+print("")
+#####
+print("x.push('Freddy') | Push Freddy onto the stack:")
+x.push("Freddy")
+#####
+print("")
+#####
+print("x.peek() | Peek at the top of the stack :")
+print(x.peek())
+#####
+print("")
+#####
+print("x.pop() | Pop the last entity add to the stack LIFO :")
+print(x.pop())
+#####
+print("")
+#####
+print("x.peek() | Peek at the top of the stack:")
+print(x.peek())
+#####
+print("")
+#####
+print("x.size() | To get the size of the stack:")
+print(x.size())
