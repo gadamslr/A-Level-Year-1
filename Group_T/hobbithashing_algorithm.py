5896701b974fe0a722cf8d47b60a4ee2d005e328
Yellow-PowerRanger <cameron4101999@gmail.com>
2016-11-24 15:05:29 +0000

    Update and rename hashing_algorithm.py to hobbithashing_algorithm.py
    The hobbit

--- Group_T/hobbithashing_algorithm.py
+++ Group_T/hobbithashing_algorithm.py
@@ -1,5 +1,5 @@
 #james
-
++print("a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort")
 stuDict = {}
 keepGoing = True
 slots = int(input("The numbers of SLOTS in your array /  data table:"))
