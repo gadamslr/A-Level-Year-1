23a2123da94660f22b832aed36ba9db9f6968e5e
gadamslr <gadams@longroad.ac.uk>
2016-11-24 14:47:00 +0000

    Initial Hash table

---
+++ Group_T/hashing_algorithm.py
@@ -0,0 +1,50 @@
+stuDict = {}
+keepGoing = True
+slots = int(input("The numbers of SLOTS in your array /  data table:"))
+
+def addStudents():
+    global slots
+    count = 0
+    tempDict = {}
+    stuName = input("Enter the name of a student:")
+
+    for element in list(stuName):
+        count += ord(element)
+
+    print(count)
+
+    index = count % slots
+
+    print("index = ", index)
+
+    tempDict[index] = stuName
+
+    print(tempDict)
+
+    return(tempDict)
+
+
+def outputStuents(stuDict):
+    print(stuDict)
+
+def menu():
+    choice = int(input("Choose from the following options:\n\
+1 - Add a new student\n\
+2 - View students in array\n\
+3 - Exit\n\
+:"))
+    if choice == 1:
+        stuDict.update(addStudents())
+    elif choice == 2:
+        outputStuents(stuDict)
+    elif choice == 3:
+        return(False)
+
+    return(True)
+
+while keepGoing == True:
+    keepGoing = menu()
+
+
+
+
